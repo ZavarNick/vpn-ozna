1aff183ed2a7bfcd11c2f172dd3d04d00c405409
ZavarNick <emelyanoffon@gmail.com>
2024-05-04 21:32:41 +0500

    Добавил коментарии к create_route.py

--- create_route.py
+++ create_route.py
@@ -3,6 +3,7 @@ import os
 
 
 def get_ip():
+    '''Узнаем локальный IP адрес VPN подключения '''
     result = subprocess.Popen(
         f'powershell.exe (Get-NetIPAddress -InterfaceAlias "VPN-OZNA" -AddressFamily IPv4).IPAddress', shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -11,18 +12,21 @@ def get_ip():
 
 
 def split_tunneling_switch():
+    '''Отключаем использование общего шлюза'''
     result = subprocess.Popen(
         f'powershell.exe Set-VpnConnection -Name "VPN-OZNA" -SplitTunneling $True', shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
 def make_route():
+    '''Добавление постоянного маршрута'''
     result = subprocess.Popen(
         f'route -p add {os.getenv('IP_NETWORK')} mask {os.getenv('MASK')} {get_ip()}', shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
 def delete_route():
+    '''Удаление старого постоянного маршрута'''
     result = subprocess.Popen(
         f'route delete {os.getenv('IP_NETWORK')}', shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
